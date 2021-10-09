e1ec4ed67a809ed385ac8c09c15b404884666509
pathanshakil8 <shakil.pathan@oodles.io>
2021-10-09 13:52:10 +0530

    3 files modified

--- home/home.controller.js
+++ home/home.controller.js
@@ -3,6 +3,39 @@ class HomeCtrl {
     'ngInject';
 
     this.name = "AngularJS";
+    this.candidates = [{
+      "id": 1,
+      "first_name": "Jeanette",
+      "last_name": "Penddreth",
+      "email": "jpenddreth0@census.gov",
+      "gender": "Female"
+    }, {
+      "id": 2,
+      "first_name": "Giavani",
+      "last_name": "Frediani",
+      "email": "gfrediani1@senate.gov",
+      "gender": "Male"
+    }, {
+      "id": 3,
+      "first_name": "Noell",
+      "last_name": "Bea",
+      "email": "nbea2@imageshack.us",
+      "gender": "Female"
+    }, {
+      "id": 4,
+      "first_name": "Willard",
+      "last_name": "Valek",
+      "email": "wvalek3@vk.com",
+      "gender": "Male"
+    }]
+
+    $scope.submit = function() {
+      if ($scope.first_name) {
+        $scope.candidates.push(this.first_name);
+        $scope.first_name = '';
+      }
+    };
+  
   }
 }
 
